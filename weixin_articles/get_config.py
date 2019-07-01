ecb51a95bd80d84b89400404b2ded6170e626e89
caiyunbin <caiyunbinlihai@163.com>
2019-07-01 16:12:29 +0800

    知乎

--- weixin_articles/get_config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-MONGO_URL = 'localhost'
-MONGO_DB ='weixin1'
-USERNAME ='caiyunbinlihai@163.com'
-PASSWORD ='caiyunbin3344'
-
-PAGE_NUMBER = 2671
-KEYWORD = '人民日报'
-cookiejar = {}
